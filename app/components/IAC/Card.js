9c71b8e5140f6bba02a85d9332c3f91d5523a3fe
Divij <divijkathuria51714@gmail.com>
2023-06-17 00:26:10 +0530

    added images and animation

--- app/components/IAC/Card.js
+++ app/components/IAC/Card.js
@@ -5,15 +5,19 @@ import Image from "next/image";
 
 export default function Card(props){
     return(
-        <motion.div className="flex justify-start mx-10 my-4 shadow-lg p-8 rounded-md"
+        <motion.div className="flex justify-start mx-12 my-4 shadow-lg  rounded-md"
         whileHover={{ scale: 1.1 }}
         transition={{ duration: 0.3 }}
         whileTap={{ scale: 0.9 }}
         >
-            <div>
-                <Image/>
+            <div className=" relative h-[150px] w-[150px] rounded-md">
+                <Image
+                src={props.image}
+                alt="prof pic"
+                fill={true}
+                />
             </div>
-            <div>
+            <div className="m-4 flex flex-col gap-2 lg:gap-4 text-sm lg:text-base">
                 <h1>{props.name}</h1>
                 <h1>{props.nation}</h1>
                 <h2>{props.uni}</h2>
--- app/components/IAC/page.js
+++ app/components/IAC/page.js
@@ -3,11 +3,14 @@ import Card from "./Card";
 import data from "./data";
 export default function IAC(){
     const cards = data.map(i=>
-        <Card key = {i.id} name = {i.name} nation = {i.nation} uni = {i.university}/>
+        <Card key = {i.id} name = {i.name} nation = {i.nation} uni = {i.university} image = {i.image}/>
         )
     return(
-        <main className="grid md:grid-cols-2 gap-10 align-center bg-white h-full text-black">
+        <body className="align-center bg-white h-full text-black flex flex-col">
+        <h1 className="flex justify-center items-center text-xl lg:text-7xl m-4">International Advisory Committee</h1>
+        <main className="grid md:grid-cols-2 gap-10 align-center  h-full text-black">
             {cards}
         </main>
+        </body>
     )
 }
